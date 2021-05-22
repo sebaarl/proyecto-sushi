3c53d8fae8de4c124d2d4a4500876f082ecf2361
Sebastian Rivera <rsebadb@gmail.com>
2021-05-22 19:21:54 -0400

    experimento

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-//Mostar menú
-const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId)
-  nav = document.getElementById(navId)
-  
-  if (toggle && nav) {
-      toggle.addEventListener('click', () => {
-          nav.classList.toggle('show-menu')
-      })
-  }
-}
-
-showMenu('nav-toggle', 'nav-menu')
-
-//Remover menú
-const navLink = document.querySelectorAll('.nav__links')
-
-function linkAction() {
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show-menu')
-}
-
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
 //Mostrar scroll top
 function scrollTop() {
   const scrollTop = document.getElementById('scroll-top')
